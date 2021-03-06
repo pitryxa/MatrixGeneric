17ef28d2bef34c9ef411a7aec7f22102ae403fb0
pboldare <petr.boldarev@t-systems.com>
2021-03-06 19:19:27 +0300

    commit

--- src/java/matrix/AdditionMatrix.java
+++ src/java/matrix/AdditionMatrix.java
@@ -8,19 +8,17 @@ public class AdditionMatrix<T extends Number> extends Matrix<T> {
             throw new IllegalArgumentException("Both matrices must have the same number of rows and columns.");
         }
         this.matrix = addition(aMatrix, bMatrix);
-
-
     }
 
     @SuppressWarnings("unchecked")
     protected T[][] addition(Matrix<T> aMatrix, Matrix<T> bMatrix) {
-        T[][] data = (T[][]) new Number[rows][cols];
+        Double[][] data = new Double[rows][cols];
         for (int i = 0; i < rows; i++) {
             for (int j = 0; j < cols; j++) {
-                data[i][j] = aMatrix.matrix[i][j] + bMatrix.matrix[i][j];
+                data[i][j] = (aMatrix.matrix[i][j].doubleValue() + bMatrix.matrix[i][j].doubleValue());
             }
         }
-        return data;
+        return (T[][])data;
     }
 
     protected boolean validate(Matrix<T> aMatrix, Matrix<T> bMatrix) {
